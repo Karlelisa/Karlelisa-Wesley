4fac7ac8b485d8dc1023b808ffa68547e8915b53
Karlelisa <karlelisaw@gmail.com>
2019-12-29 15:40:07 -0500

    adding files

---
+++ js/portfolio.js
@@ -0,0 +1,31 @@
+// Cited: https://codepen.io/SitePoint/pen/yXWXaw
+// The 3D carousel
+
+$(document).ready(() => {
+    let
+        carousel = document.querySelector('.carousel'),
+        figure = carousel.querySelector('figure'),
+        nav = carousel.querySelector('nav'),
+        numImages = figure.childElementCount,
+        theta = 2 * Math.PI / numImages,
+        currImage = 0;
+
+    nav.addEventListener('click', onClick, true);
+
+    function onClick(e) {
+        e.stopPropagation();
+
+        let t = e.target;
+        if (t.tagName.toUpperCase() != 'BUTTON')
+            return;
+
+        if (t.classList.contains('next')) {
+            currImage++;
+        } else {
+            currImage--;
+        }
+
+        figure.style.transform = `rotateY(${currImage * -theta}rad)`;
+    }
+
+});
